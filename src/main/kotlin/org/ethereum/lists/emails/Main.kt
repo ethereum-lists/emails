32f113dd51102a797dccd00d58f4786dfa4d06e1
ligi <ligi@ligi.de>
2018-02-16 00:03:11 +0100

    Add script to check emails

---
+++ src/main/kotlin/org/ethereum/lists/emails/Main.kt
@@ -0,0 +1,22 @@
+package org.ethereum.lists.emails
+
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.Parser
+import java.io.File
+
+
+val EMAIL_REGEX = Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+
+fun main(args: Array<String>) {
+    File("emails-darklist.json").toJSONArray()
+            .map {  it as String }
+            .forEach {
+                println("checking $it")
+                if (!EMAIL_REGEX.matches(it)) {
+                    throw IllegalArgumentException("Invalid email: " + it)
+                }
+            }
+
+}
+
+fun File.toJSONArray() = Parser().parse(this.reader()) as JsonArray<*>
